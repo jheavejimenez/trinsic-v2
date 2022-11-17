be2839c38880d4c22cd49ab22df10c6e683e6e94
jheave <jimenezjheave123@gmail.com>
2022-11-17 13:43:32 +0800

    refactor

--- routes/requests.js
+++ routes/requests.js
@@ -1,7 +1,48 @@
-const { IssueFromTemplateRequest, LoginRequest } = require("@trinsic/trinsic");
+const { IssueFromTemplateRequest, LoginRequest, TrinsicService, EcosystemInfoRequest, InsertItemRequest } = require("@trinsic/trinsic");
 const router = require('express').Router();
 let Requests = require('../models/requests');
 const User = require("../models/users");
+const trinsic = new TrinsicService();
+
+async function getEcoSystemId() {
+    trinsic.setAuthToken(process.env.AUTHTOKEN || "");
+
+    const infoResponse = await trinsic
+        .provider()
+        .ecosystemInfo(EcosystemInfoRequest.fromPartial({}));
+
+    const ecosystem = infoResponse.ecosystem;
+
+    return ecosystem?.id;
+}
+
+async function loginAnonymous({ email }) {
+    trinsic.setAuthToken(process.env.AUTHTOKEN || "");
+
+    const walletAuth = await trinsic.account().loginAnonymous(await getEcoSystemId());
+    const newUser = new User({ email, walletAuth });
+    await newUser.save();
+}
+
+async function issueCredential({ templateId, valuesJson }) {
+    trinsic.setAuthToken(process.env.AUTHTOKEN || "");
+
+    const issueResponse = await trinsic.credential().issueFromTemplate(
+        IssueFromTemplateRequest.fromPartial({
+            templateId,
+            valuesJson
+        })
+    );
+
+    return issueResponse.documentJson
+}
+
+async function storeCredential(itemJson) {
+    // get the user's wallet auth
+    const insertResponse = await trinsic.wallet().insertItem(
+        InsertItemRequest.fromPartial({ itemJson })
+    );
+}
 
 router.route('/').get(async (req, res) => {
     const requests = await requests.find();
@@ -15,6 +56,11 @@ router.route('/').get(async (req, res) => {
         orgName
     } = req.body;
 
+    const user = await User.findOne({ email: email });
+
+    if (!user) {
+        await loginAnonymous({ email });
+    }
     // save request to mongodb
     const request = new Requests({
         email,
@@ -31,8 +77,13 @@ router.route('/').get(async (req, res) => {
 });
 
 router.route('/:id/issue').put(async (req, res) => {
-    // create trinsic account
-    
+    const credentialValues = JSON.stringify({
+        firstName: "Allison",
+        lastName: "Allisonne",
+        batchNumber: "123454321",
+        countryOfVaccination: "US",
+    });
+
 })
 
 module.exports = router;
